74b958795e8e6d9846ce4c1b23b179ce2b2f9381
David Anderson <davidias.j.a@gmail.com>
2022-11-02 19:26:04 -0400

    Add login activity and page

---
+++ app/src/main/java/com/example/group28assignmentapp/database/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.group28assignmentapp.database;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.group28assignmentapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedStateInstance) {
+        super.onCreate(savedStateInstance);
+        setContentView(R.layout.activity_login);
+    }
+
+}
