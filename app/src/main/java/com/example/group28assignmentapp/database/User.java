2b6e7b2b7f8dedf1cb00c4b6d7bf76a457c7ddb1
David Anderson <davidias.j.a@gmail.com>
2022-11-03 07:11:25 -0400

    Additions to User and Sticker class

--- app/src/main/java/com/example/group28assignmentapp/database/User.java
+++ app/src/main/java/com/example/group28assignmentapp/database/User.java
@@ -1,8 +1,12 @@
 package com.example.group28assignmentapp.database;
 
+import java.util.List;
+
 public class User {
-    public String username;
-    public String message;
+    private String username;
+    private String message;
+    private List<Sticker> received;
+    private List<Sticker> sent;
 
 
     public User(){
@@ -12,5 +16,34 @@ public class User {
     public User(String username, String message){
         this.username = username;
         this.message = message;
+    }
+
+    /*
+    Public getters for fields allow us to add custom User Objects to the Realtime DB
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<Sticker> getReceived() {
+        return received;
+    }
+
+    public List<Sticker> getSent() {
+        return sent;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", message='" + message + '\'' +
+                ", received=" + received +
+                ", sent=" + sent +
+                '}';
     }
 }
