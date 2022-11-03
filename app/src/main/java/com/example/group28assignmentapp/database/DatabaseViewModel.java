88ce1466ae006ca05c66f7d1cc0f0a37960ef18c
David Anderson <davidias.j.a@gmail.com>
2022-11-02 20:30:37 -0400

    Update viewModel to get users as a Map

--- app/src/main/java/com/example/group28assignmentapp/database/DatabaseViewModel.java
+++ app/src/main/java/com/example/group28assignmentapp/database/DatabaseViewModel.java
@@ -1,20 +1,45 @@
 package com.example.group28assignmentapp.database;
 
+import android.util.Log;
+
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class DatabaseViewModel extends ViewModel {
     private ArrayList<String> listOfUsernames;
     private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    private final String TAG = "REALTIME-DATABASE";
 
     public void loadUsernames() {
         //TODO: load the usernames from the database into listOfUsernames so we can
         // check if the one the user enters exists, or if the user wants to create a NEW user,
         // we must check that that username doesn't already exist
+
+        // Want app data to update in real time when the databse updates.
+        // To do this, we add an event listener the the mUsers reference above:
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                Log.d(TAG, "Value is: " + map.get("users"));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 
 
