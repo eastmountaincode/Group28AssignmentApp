ad6de67942b73d3d22cb728aefad7bbd3aaca49a
David Anderson <davidias.j.a@gmail.com>
2022-11-02 20:22:30 -0400

    Revert to launching databasemainactivity

--- app/src/main/java/com/example/group28assignmentapp/MainActivity.java
+++ app/src/main/java/com/example/group28assignmentapp/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         binding.firebaseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                startActivity(new Intent(MainActivity.this, DatabaseMainActivity.class));
             }
         });
 
