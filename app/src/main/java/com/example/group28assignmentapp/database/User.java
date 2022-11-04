8ed4b9dd6beb772b8be93d90c2e1bbc03398bf5a
djanders19 <davidias.j.a@gmail.com>
2022-11-04 16:10:05 -0400

    Update User to match DB

--- app/src/main/java/com/example/group28assignmentapp/database/User.java
+++ app/src/main/java/com/example/group28assignmentapp/database/User.java
@@ -1,46 +1,57 @@
 package com.example.group28assignmentapp.database;
 
-
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
     private String username;
-    private List<Sticker> sent;
     private List<Sticker> received;
+    private List<Sticker> sent;
 
-    public User() {
+    public User(){
         // Default constructor necessary for calls to DataSnapshot.getValue(User.class)
     }
 
-    public User(String username,
-                List<Sticker> sentStickers,
-                List<Sticker> receivedStickers) {
+    public User(String username){
         this.username = username;
-        this.received = receivedStickers;
-        this.sent = sentStickers;
+        this.received = new ArrayList<>();
+        this.sent = new ArrayList<>();
     }
 
+    /*
+    Public getters for fields allow us to add custom User Objects to the Realtime DB
+     */
     public String getUsername() {
         return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public List<Sticker> getSent() {
-        return sent;
-    }
-
-    public void setSent(List<Sticker> sent) {
-        this.sent = sent;
     }
 
     public List<Sticker> getReceived() {
         return received;
     }
 
+    public List<Sticker> getSent() {
+        return sent;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public void setReceived(List<Sticker> received) {
         this.received = received;
+    }
+
+    public void setSent(List<Sticker> sent) {
+        this.sent = sent;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", received=" + received +
+                ", sent=" + sent +
+                '}';
     }
 }
