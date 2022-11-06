7b5ebcf693b91ff379820869e025772ac758bd41
David Anderson <davidias.j.a@gmail.com>
2022-11-06 06:41:36 -0500

    Added method and data stubs to UserViewModel

--- app/src/main/java/com/example/group28assignmentapp/database/model/UserViewModel.java
+++ app/src/main/java/com/example/group28assignmentapp/database/model/UserViewModel.java
@@ -5,14 +5,32 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.group28assignmentapp.database.User;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
 public class UserViewModel extends ViewModel {
     private final MutableLiveData<List<String>> _listOfUsernames = new MutableLiveData<>();
-    public final LiveData<List<String>> listOfUsernames = _listOfUsernames;
+    public final LiveData<List<String>> listOfUsernames = _listOfUsernames;  // observable
 
-    private final MutableLiveData<User> _user = new MutableLiveData<>();
-    public final LiveData<User> user = _user;
+    private final MutableLiveData<User> _user = new MutableLiveData<>();  // Mutable. Change this.
+    public final LiveData<User> user = _user;  // Observable. Not Mutable.
+
+    // References to the Realtime DB:
+    private final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("users3");
+    private final String TAG = "REALTIME-DATABASE";
+
+    public void mListenToDatabase() {
+
+    }
+
+    public void attemptToLoginUsername(String username) {
+        // Ensure that database connection is setup
+        // Check if a user with username exists
+        // Set the logged in user to the User with the username username
+    }
+
+
 
 }
