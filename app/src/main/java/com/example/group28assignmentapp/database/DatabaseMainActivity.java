de7b5ef40c16e276f9837ef4fd464c534356a1e5
David Anderson <davidias.j.a@gmail.com>
2022-11-06 05:54:57 -0500

    Starting from scratch-ish

--- app/src/main/java/com/example/group28assignmentapp/database/DatabaseMainActivity.java
+++ app/src/main/java/com/example/group28assignmentapp/database/DatabaseMainActivity.java
@@ -4,18 +4,22 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.group28assignmentapp.R;
 
-public class DatabaseMainActivity extends AppCompatActivity implements MessageViewActivity.OnFragmentInteractionListener{
+public class DatabaseMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.database_activity_main);
-    }
 
-    @Override
-    public void onFragmentInteraction(Uri url) {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.messageFragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
 
+        NavigationUI.setupActionBarWithNavController(this, navController);
     }
 }
