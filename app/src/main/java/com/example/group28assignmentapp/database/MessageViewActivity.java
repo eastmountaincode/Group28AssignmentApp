488bb42d210cf8adb04c220fea313bd691519968
Yikan Zhang <zhang.yikan1@northeastern.edu>
2022-11-06 11:48:11 -0500

    exit confirmation

--- app/src/main/java/com/example/group28assignmentapp/database/MessageViewActivity.java
+++ app/src/main/java/com/example/group28assignmentapp/database/MessageViewActivity.java
@@ -1,7 +1,10 @@
 package com.example.group28assignmentapp.database;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.example.group28assignmentapp.R;
@@ -12,5 +15,20 @@ public class MessageViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_view);
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to log out?").setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                        .setNegativeButton("No",null)
+                                .show();
+
     }
 }
