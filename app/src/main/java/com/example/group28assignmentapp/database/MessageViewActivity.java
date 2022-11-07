5e2c5cdf2deb257caa9c12953049a6f9a9d501a8
David Anderson <davidias.j.a@gmail.com>
2022-11-06 19:59:02 -0500

    Skeleton code for notifications

--- app/src/main/java/com/example/group28assignmentapp/database/MessageViewActivity.java
+++ app/src/main/java/com/example/group28assignmentapp/database/MessageViewActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.group28assignmentapp.R;
@@ -18,11 +21,26 @@ public class MessageViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        createNotificationChannel();
         username = getIntent().getStringExtra("USERNAME");
         sharedViewModel = new ViewModelProvider(this).get(MessageViewModel.class);
         sharedViewModel.setUsername(username);
         setContentView(R.layout.activity_message_view);
 
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(getString(R.string.channel_id), name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     @Override
--- app/src/main/java/com/example/group28assignmentapp/database/ReceivedListFragment.java
+++ app/src/main/java/com/example/group28assignmentapp/database/ReceivedListFragment.java
@@ -1,11 +1,16 @@
 package com.example.group28assignmentapp.database;
 
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -55,29 +60,40 @@ public class ReceivedListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_received_list, container, false);
         stickerList = new ArrayList<>();
 
+        // Setup the recycler view
         recyclerView = view.findViewById(R.id.received_recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
         DatabaseRecyclerAdapter adapter = new DatabaseRecyclerAdapter(getContext(), (ArrayList<Sticker>) stickerList);
         recyclerView.setAdapter(adapter);
 
+        // Get username from the viewmodel and ask for pertinent user info rom the database:
         username = sharedViewModel.getUsername();
         mDatabase = FirebaseDatabase.getInstance().getReference("users3/" + username + "/received");
+
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // We will be updating the UI to reflect the contents of stickerList, so need to
+                // clear old entries if there are any:
                 stickerList.clear();
 
+                // If no stickers have been received, the snapshot value will be null
                 if ((snapshot.getValue() == null)){
                     return;
                 }
+
+                // Every child of the snapshot is a Sticker. We want to add all these stickers to
+                // our list of stickers
                 for (DataSnapshot shot : snapshot.getChildren()){
                     Sticker sticker = shot.getValue(Sticker.class);
                     stickerList.add(sticker);
                 }
 
+                // Update the recyclerview in the UI:
                 adapter.notifyDataSetChanged();
+
+                notifyNewMessageReceived();
             }
 
             @Override
@@ -86,10 +102,25 @@ public class ReceivedListFragment extends Fragment {
             }
         });
 
-
-
-
-
         return view;
     }
+
+    public void notifyNewMessageReceived() {
+        // Build notification:
+        String channelID = getString(R.string.channel_id);
+        Notification notif = new NotificationCompat.Builder(this.getContext(), channelID)
+                .setContentTitle("New Sticker!")
+                .setContentText("Someone sent you a sticker - find out who!")
+                .setSmallIcon(R.drawable.clark)
+                .build();
+
+        // Get the notification service from the system:
+        NotificationManager notifManager = (NotificationManager) this.getContext()
+                .getSystemService(NOTIFICATION_SERVICE);
+        // hide the notification after its selected
+        notif.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        notifManager.notify(0, notif);
+    }
+
 }
