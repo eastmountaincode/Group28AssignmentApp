4119c086628fad73d124f1d901e138939a52a1b1
David Anderson <davidias.j.a@gmail.com>
2022-11-07 08:20:04 -0500

    Update notifications to display the sticker received

--- app/src/main/java/com/example/group28assignmentapp/database/ReceivedListFragment.java
+++ app/src/main/java/com/example/group28assignmentapp/database/ReceivedListFragment.java
@@ -4,6 +4,8 @@ import static android.content.Context.NOTIFICATION_SERVICE;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,6 +28,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ReceivedListFragment extends Fragment {
@@ -35,6 +38,11 @@ public class ReceivedListFragment extends Fragment {
     private MessageViewModel sharedViewModel;
     private String username;
     private ArrayList<Sticker> stickerList;
+
+    // Store the last stickers seen so we can identify which sticker is new when a new
+    // sticker is received:
+    private ArrayList<Sticker> lastStickerList;
+    boolean initialLoad = true;
 
 
     public ReceivedListFragment() {
@@ -58,6 +66,7 @@ public class ReceivedListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_received_list, container, false);
         stickerList = new ArrayList<>();
+        lastStickerList = new ArrayList<>();
 
         // Setup the recycler view
         recyclerView = view.findViewById(R.id.received_recycler_view);
@@ -75,6 +84,7 @@ public class ReceivedListFragment extends Fragment {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 // We will be updating the UI to reflect the contents of stickerList, so need to
                 // clear old entries if there are any:
+                lastStickerList = (ArrayList<Sticker>) stickerList.clone();
                 stickerList.clear();
 
                 // If no stickers have been received, the snapshot value will be null
@@ -82,17 +92,28 @@ public class ReceivedListFragment extends Fragment {
                     return;
                 }
 
+                Sticker newSticker = new Sticker();
                 // Every child of the snapshot is a Sticker. We want to add all these stickers to
                 // our list of stickers
                 for (DataSnapshot shot : snapshot.getChildren()) {
                     Sticker sticker = shot.getValue(Sticker.class);
+                    if (!initialLoad && !lastStickerList.contains(sticker)) {
+                        newSticker = sticker;
+                    }
                     stickerList.add(sticker);
                 }
 
                 // Update the recyclerview in the UI:
                 adapter.notifyDataSetChanged();
 
-                notifyNewMessageReceived();
+                if (!initialLoad) {
+                    // Don't send notifications on first read of database
+                    notifyNewMessageReceived(newSticker);
+                }
+
+                if (initialLoad) {
+                    initialLoad = false;
+                }
             }
 
             @Override
@@ -105,13 +126,22 @@ public class ReceivedListFragment extends Fragment {
     }
 
     // Method adapted from example code provided in class:
-    public void notifyNewMessageReceived() {
+    public void notifyNewMessageReceived(Sticker sticker) {
+
+        // Get Bitmap of sticker received so that we can display it in the notif:
+        int id = getContext().getResources().getIdentifier(sticker.getSticker_number(),
+                "drawable",
+                getContext().getPackageName());
+        Bitmap icon = BitmapFactory.decodeResource(getContext().getResources(),
+                id);
+
         // Build notification:
         String channelID = getString(R.string.channel_id);
         Notification notif = new NotificationCompat.Builder(this.getContext(), channelID)
                 .setContentTitle("New Sticker!")
                 .setContentText("Someone sent you a sticker - find out who!")
-                .setSmallIcon(R.drawable.clark)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setLargeIcon(icon)
                 .build();
 
         // Get the notification service from the system:
